5239b76f9a09e8ee1306ba963bbb9e024e6ac87a
Omar Loyo <omarloyo121@gmail.com>
2022-10-24 22:57:47 -0600

    import script completed, update cases on new import pending

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -3,8 +3,24 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
+  const kk = await prisma.customer.createMany(
+    {
+      data: [
+        { name: "Advantage Metals Recycling", alt: "AMR" },
+        { name: "David J Joseph Co", alt: "DJJ" },
+        { name: "Ferrous Charleston", alt: "Fe" },
+        { name: "River Metals Recycling", alt: "RMR" },
+        { name: "Texas Port Recycling", alt: "TPR" },
+        { name: "Trademark Metals Recycling", alt: "TMR" },
+        { name: "U-Pull and Pay", alt: "UPAP" },
+        { name: "U-Pull-&-Pay", alt: "UPAP" },
+        { name: "Western Metals Recycling", alt: "WMR" },
+      ]
+    }
+  )
+  
+  res.send(kk) 
+  
 };
 
 export default examples;
--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -2,91 +2,129 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 import fs from "fs";
 import { parse } from "csv-parse";
+import path from "path";
 
-const customers: Customer[] = [];
-const sites: Site[] = [];
-const cases: Case[] = [];
+const upload = async (req: NextApiRequest, res: NextApiResponse) => {
+  // console.log(req);
+  const sites: Site[] = [];
+  const cases: Case[] = [];
 
-// customers = [
-//   { customer: "Advantage Metals Recycling", id: 1 },
-//   { customer: "David J Joseph Co", id: 2 },
-//   { customer: "Ferrous Charleston", id: 3 },
-//   { customer: "River Metals Recycling", id: 4 },
-//   { customer: "Texas Port Recycling", id: 5 },
-//   { customer: "Trademark Metals Recycling", id: 6 },
-//   { customer: "U-Pull and Pay", id: 7 },
-//   { customer: "U-Pull-&-Pay", id: 8 },
-//   { customer: "Western Metals Recycling", id: 9 },
-// ];
+  const customers = await prisma.customer.findMany();
 
-interface Customer {
-    id: string;
+  // customers = [
+  //   { customer: "Advantage Metals Recycling", id: 1 },
+  //   { customer: "David J Joseph Co", id: 2 },
+  //   { customer: "Ferrous Charleston", id: 3 },
+  //   { customer: "River Metals Recycling", id: 4 },
+  //   { customer: "Texas Port Recycling", id: 5 },
+  //   { customer: "Trademark Metals Recycling", id: 6 },
+  //   { customer: "U-Pull and Pay", id: 7 },
+  //   { customer: "U-Pull-&-Pay", id: 8 },
+  //   { customer: "Western Metals Recycling", id: 9 },
+  // ];
+
+  interface Site {
     name: string;
-    alt: string;
-}
+    customerId: string;
+  }
 
-interface Site {
-    name: string;
-    customer_id?: string;
-}
-
-interface Case {
+  interface Case {
     id: string;
     date: string;
     status: string;
     queue: string;
-    alt: string;
-    site: string;
+    site?: string;
+    siteId: string;
     subject: string;
     stealthNotes: string;
     notes: string;
     atention: boolean;
-}
+  }
 
+  fs.writeFile("./public/cases.csv", req.body, (err) => {
+    res.send(err);
+  });
 
+  fs.createReadStream("./public/cases.csv")
+    .pipe(parse({ delimiter: ",", columns: true }))
+    .on("data", (row: any) => {
+      if (!sites.some((site) => site.name === row.Site)) {
+        sites.push({
+          name: row.Site,
+          customerId: "",
+        });
+      }
 
-fs.createReadStream("Cases_DJJ 09-28-2022.csv")
-  .pipe(parse({ delimiter: ",", columns: true }))
-  .on("data", (row) => {
-    if (!sites.some((site) => site.name === row.Site)) {
-      sites.push({ name: row.Site });
-    }
+      cases.push({
+        id: row["Case Number"],
+        date: row["Case Date"],
+        status: row["Case Status"],
+        queue: row["Queue"],
+        site: row.Site,
+        siteId: "",
+        subject: row["Case Subject"],
+        stealthNotes: "",
+        notes: "",
+        atention: true,
+      });
+    })
+    .on("end", async () => {
+      sites.sort((a, b) => (a.name > b.name ? 1 : -1));
 
-    cases.push({
-      id: row["Case Number"],
-      date: row["Case Date"],
-      status: row["Case Status"],
-      queue: row["Queue"],
-      alt: "",
-      site: row.Site,
-      subject: row["Case Subject"],
-      stealthNotes: "",
-      notes: "",
-      atention: true,
-    });
-  })
-  .on("end", () => {
-    sites.sort((a, b) => (a.name > b.name ? 1 : -1));
-
-    for (let site of sites) {
-      for (let customer of customers) {
-        if (site.name.includes(customer.name)) {
-          if (customer.name === "David J Joseph Co") {
-            site.customer_id = customer.id;
-          } else {
-            site.name = (site.name.split(customer.name)[1] || "").substring(1);
-            site.customer_id = customer.id;
+      for (let site of sites) {
+        for (let customer of customers) {
+          if (site.name.includes(customer.name)) {
+            if (customer.name === "David J Joseph Co") {
+              site.customerId = customer.id;
+            } else {
+              site.name = (site.name.split(customer.name)[1] || "").substring(
+                1
+              );
+              site.customerId = customer.id;
+            }
           }
         }
       }
-    }
 
-    for (let siteCase of cases) {
-      for (let site of sites) {
-        if (siteCase.site.includes(`${site.name}`)) {
-          siteCase.site = site.name;
-          // siteCase.customer_id = customers.find(customer => customer.id === site.customer_id).customer;
+      try {
+        const sitesWhithCustomer = await prisma.site.createMany({
+          data: sites,
+          skipDuplicates: true,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
+      const dbSites = await prisma.site.findMany();
+
+      for (let siteCase of cases) {
+        for (let site of dbSites) {
+          if (siteCase.site?.includes(`${site.name}`)) {
+            siteCase.siteId = site.id;
+            delete siteCase.site;
+          }
         }
       }
-    }
-  });
+
+      try {
+        const casesWithSite = await prisma.cases.createMany({
+          data: cases,
+          skipDuplicates: true,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+  res.send("Imported");
+};
+
+export default upload;
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "5mb", // Set desired value here
+    },
+  },
+};
