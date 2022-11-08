9928c8663d039a7d06e418a21d3d0fca7181e758
Omar Loyo <omarloyo121@gmail.com>
2022-11-07 20:03:37 -0600

    header finished

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+import styles from './Header.module.css';
+
+interface Props {
+    title: string;
+}
+
+const Header = ({ title }:Props) => {
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.search}>
+        <input type="text" />
+        <AiOutlineSearch color='#22181C' size={18}/>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
