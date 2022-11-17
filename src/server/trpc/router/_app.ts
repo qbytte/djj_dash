f3b2ac4bc65f4a5f0112edc502aa4275432b05f7
Omar Loyo <78561511+qbytte@users.noreply.github.com>
2022-11-17 15:32:08 -0600

    Update _app.ts

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 import { categories } from "./categories";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   categories: categories,
 });
